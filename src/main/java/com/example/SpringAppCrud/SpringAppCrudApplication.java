d9c577e3cc69b4cf9eb1df1da76ebd5af2a6a32f
RazvanCiubotaru23 <razvanciubotaru87@gmail.com>
2024-10-05 13:42:59 +0300

    First Commit

---
+++ src/main/java/com/example/SpringAppCrud/SpringAppCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringAppCrud/SpringAppCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
